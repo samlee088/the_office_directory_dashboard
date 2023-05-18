db491e50483f6c5ada01157cf0650cde8bda5370
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-05-17 18:42:26 -0700

    update with quotes buttons

--- client/src/scenes/quotes/index.jsx
+++ client/src/scenes/quotes/index.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios.js';
+import { Box} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import ButtonBase from '@mui/material/ButtonBase';
+import Typography from '@mui/material/Typography';
+import dwightAndJimImage from 'images/dwightandjim.jpg'
+import pamAndJimImage from 'images/jimandpampicture.jpg'
+import QuotesButtons from 'components/QuotesButtons';
 
 const Quotes = () => {
 
@@ -16,14 +23,15 @@ const Quotes = () => {
     }
 
     fetchData();
-    console.log(quotes)
 
   }, [])
 
 
+ 
+
   return (
     <div>
-      
+    <QuotesButtons/>
     </div>
   )
 }
